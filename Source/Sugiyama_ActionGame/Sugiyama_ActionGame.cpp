db839c43b4238bf171274998acd2d2a164ab5c5f
gou-sugiyama <107020929+gou-sugiyama@users.noreply.github.com>
2022-10-24 15:01:43 +0900

    敵のAIをつくったよ歩いたよ

---
+++ Source/Sugiyama_ActionGame/Sugiyama_ActionGame.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Sugiyama_ActionGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Sugiyama_ActionGame, "Sugiyama_ActionGame" );
